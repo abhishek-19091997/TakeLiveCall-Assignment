4e8e59af718fd68db8bf6f7e8e50f9ce042b5a8d
abhishek-19091997 <abpatra000@gmail.com>
2023-05-05 11:55:31 +0530

    assignment

---
+++ src/Components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./home.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  return <div id="body">Sports App</div>;
+}
